a87ee7e021e4d78fce653b5f19fa9e882601d755
Nico Albanese <nico.albanese@vercel.com>
2024-04-12 13:33:02 +0100

    removed input to gta

--- app/examples/generate-text/action.ts
+++ app/examples/generate-text/action.ts
@@ -3,7 +3,7 @@
 import { experimental_generateText as generateText } from "ai";
 import { openai } from "ai/openai";
 
-export const generateTextAction = async (input: string) => {
+export const generateTextAction = async () => {
   const { text } = await generateText({
     model: openai.chat("gpt-3.5-turbo"),
     temperature: 0.5,
