9a9c9ca0d090124e21d627376253a12041cb11fb
Nico Albanese <nico.albanese@vercel.com>
2024-05-08 22:38:52 +0100

    update root page

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import { Link } from "@/components/link";
 export default function Page() {
   return (
     <main className="space-y-4">
-      <h1 className="text-xl font-semibold">AI SDK Core Fundamentals</h1>
+      <h1 className="text-xl font-semibold">Vercel AI SDK Fundamentals</h1>
       <p>
-        The following examples aim to showcase the fundamentals behind the new
-        Vercel AI SDK Core. The examples are very simple, with minimal loading
+        The following examples aim to showcase the fundamentals behind the
+        Vercel AI SDK. The examples have minimal loading
         states to remain as simple as possible.
       </p>
       <p>
@@ -18,10 +18,10 @@ export default function Page() {
           <Link href="/examples/generate-text">Generate Text</Link>
         </li>
         <li>
-          <Link href="/examples/generate-object">Generate Object</Link>
+          <Link href="/examples/stream-text">Stream Text</Link>
         </li>
         <li>
-          <Link href="/examples/stream-text">Stream Text</Link>
+          <Link href="/examples/generate-object">Generate Object</Link>
         </li>
         <li>
           <Link href="/examples/stream-object">Stream Object</Link>
@@ -33,7 +33,7 @@ export default function Page() {
           <Link href="/examples/basic-chatbot">Chatbot with `useChat`</Link>
         </li>
         <li>
-          <Link href="/examples/generate-ui">Generate UI</Link>
+          <Link href="/examples/generate-ui-streamui">Generate UI</Link>
         </li>
       </ul>
     </main>
