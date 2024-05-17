049484c3b60c6bc41ad301e0a0f94216aaab743b
Nico Albanese <nico.albanese@vercel.com>
2024-05-17 14:53:21 +0100

    update root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,20 @@
-import { Link } from "@/components/link";
-
-export default function Page() {
+export default function ChatPage() {
   return (
-    <main className="space-y-4">
-      <h1 className="text-xl font-semibold">Vercel AI SDK Fundamentals</h1>
-      <p>
-        The following examples aim to showcase the fundamentals behind the
-        Vercel AI SDK. The examples have minimal loading
-        states to remain as simple as possible.
-      </p>
-      <p>
-        The prompt for the first 4 examples (stream/generate text/object) is
-        `Tell me a joke`.
-      </p>
-      <ul className="list-disc list-inside">
-        <li>
-          <Link href="/examples/generate-text">Generate Text</Link>
-        </li>
-        <li>
-          <Link href="/examples/stream-text">Stream Text</Link>
-        </li>
-        <li>
-          <Link href="/examples/generate-object">Generate Object</Link>
-        </li>
-        <li>
-          <Link href="/examples/stream-object">Stream Object</Link>
-        </li>
-        <li>
-          <Link href="/examples/tools/basic">Basic Tool</Link>
-        </li>
-        <li>
-          <Link href="/examples/basic-chatbot">Chatbot with `useChat`</Link>
-        </li>
-        <li>
-          <Link href="/examples/generate-ui-streamui">Generate UI</Link>
-        </li>
-      </ul>
-    </main>
+    <div>
+      {/* {messages.map((m) => ( */}
+      {/*   <div key={m.id}> */}
+      {/*     {m.role === "user" ? "User: " : "AI: "} */}
+      {/*     {m.content} */}
+      {/*   </div> */}
+      {/* ))} */}
+
+      {/* <form onSubmit={handleSubmit}> */}
+      {/*   <input */}
+      {/*     value={input} */}
+      {/*     placeholder="Say something..." */}
+      {/*     onChange={handleInputChange} */}
+      {/*   /> */}
+      {/* </form> */}
+    </div>
   );
 }
