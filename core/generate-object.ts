829626902d5dfcf75395a671aec74e731c3c7b00
Nico Albanese <nico.albanese@vercel.com>
2024-05-22 06:35:51 -0400

    update core starter

--- core/generate-object.ts
+++
@@ -1,7 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
-async function main() {}
-
-main().catch(console.error);
--- core/tools.ts
+++ core/tools.ts
@@ -1,7 +1,15 @@
+import { openai } from "@ai-sdk/openai";
+import { generateText } from "ai";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-async function main() {}
+async function main() {
+  const location = "London";
+  const result = await generateText({
+    model: openai("gpt-4o"),
+    prompt: `You are a funny chatbot. users location: ${location}`,
+  });
+}
 
 main().catch(console.error);
